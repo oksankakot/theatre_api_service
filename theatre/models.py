7ba1f5466b0409d9dd01bc14ba1f300929d10837
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-03-24 17:57:39 +0200

    Fixed TheatreHall attribute from seats_in_rows to seats_in_row

--- theatre/models.py
+++ theatre/models.py
@@ -6,11 +6,11 @@ from django.db import models
 class TheatreHall(models.Model):
     name = models.CharField(max_length=255)
     rows = models.IntegerField()
-    seats_in_rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
 
     @property
     def capacity(self) -> int:
-        return self.rows * self.seats_in_rows
+        return self.rows * self.seats_in_row
 
     def __str__(self):
         return self.name
