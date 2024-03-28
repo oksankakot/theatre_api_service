0d286d4d8fe8790b0216540e6b6c10c86b0dcc45
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-03-28 18:22:33 +0200

    Modified settings for postgresql, made some changes to requirements.txt and .gitignore

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -89,22 +89,13 @@ WSGI_APPLICATION = "theatre_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
     }
 }
-
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "HOST": os.getenv("POSTGRES_HOST"),
-#         "NAME": os.getenv("POSTGRES_DB"),
-#         "USER": os.getenv("POSTGRES_USER"),
-#         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-#     }
-# }
-
 
 
 # Password validation
